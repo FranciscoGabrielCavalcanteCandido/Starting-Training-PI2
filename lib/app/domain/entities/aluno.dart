2c0d95174aa11caad15cd1b56049bab048f132f0
EduardoDfs <odraudesaiddfs@gmail.com>
2022-07-04 22:39:12 -0300

    Eduardo - extendendo a classe pessoa para aluno e personal

--- lib/app/domain/entities/aluno.dart
+++ lib/app/domain/entities/aluno.dart
@@ -1,5 +1,25 @@
-class Aluno {
+import 'package:starting_training/app/domain/entities/pessoa.dart';
+
+class Aluno extends Pessoa {
   int frequencia;
 
-  Aluno(this.frequencia);
+  Aluno(
+      {required String CPF,
+      required String nome,
+      required DateTime dataNascimento,
+      required int id,
+      required String telefone,
+      required String academia,
+      required String endereco,
+      required String status,
+      required this.frequencia})
+      : super(
+            CPF: CPF,
+            nome: nome,
+            dataNascimento: dataNascimento,
+            id: id,
+            telefone: telefone,
+            academia: academia,
+            endereco: endereco,
+            status: status);
 }
--- lib/app/domain/entities/personal_trainer.dart
+++ lib/app/domain/entities/personal_trainer.dart
@@ -1,8 +1,27 @@
-class PersonalTreiner {
-  int id;
+import 'package:starting_training/app/domain/entities/pessoa.dart';
+
+class PersonalTreiner extends Pessoa {
   String cref;
   DateTime validadeCref;
 
   PersonalTreiner(
-      {required this.id, required this.cref, required this.validadeCref});
+      {required this.cref,
+      required this.validadeCref,
+      required String CPF,
+      required String nome,
+      required DateTime dataNascimento,
+      required int id,
+      required String telefone,
+      required String academia,
+      required String endereco,
+      required String status})
+      : super(
+            CPF: CPF,
+            nome: nome,
+            dataNascimento: dataNascimento,
+            id: id,
+            telefone: telefone,
+            academia: academia,
+            endereco: endereco,
+            status: status);
 }
