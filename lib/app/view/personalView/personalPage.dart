18f11951250eed2474e159a1e91864483621bbf0
fg.14.cavalcandido@gmail.com <fg.14.cavalcandido@gmail.com>
2022-07-20 19:58:35 -0300

    Francisco - criando tela do personal trainer

---
+++ lib/app/view/personalView/personalPage.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class PersonalPage extends StatelessWidget {
+  const PersonalPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("Pagina Personal"),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 200,
+          left: 40,
+          right: 40,
+        ),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+              ),
+              child: FlatButton(
+                child: Text("Alunos"),
+                onPressed: () =>
+                    Navigator.popAndPushNamed(context, "/listAlunoPage"),
+              ),
+            ),
+            Container(
+              height: 30,
+            ),
+            Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+              ),
+              child: FlatButton(
+                child: Text("Treinos"),
+                onPressed: () => Navigator.popAndPushNamed(
+                    context, "/registrationTreinoPage"),
+              ),
+            ),
+            Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+              ),
+              child: FlatButton(
+                child: Text("Relatórios"),
+                onPressed: () => Navigator.popAndPushNamed(
+                    context, "/generationRelatorioPage"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
