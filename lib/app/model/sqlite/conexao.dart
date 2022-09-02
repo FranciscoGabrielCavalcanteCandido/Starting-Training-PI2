8228efd63bb7afbe0b60f48ae04f3c6c1bd2c475
FranciscoGabrielCavalcanteCandido <fg.14.cavalcandido@gmail.com>
2022-09-01 22:35:46 -0300

    Francisco - conexao

---
+++ lib/app/model/sqlite/conexao.dart
@@ -0,0 +1,24 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:starting_training/app/model/sqlite/conexao.dart';
+
+class Conexao {
+  static Database? _conexao;
+
+  static Future<Database> getConexao() async {
+    if (_conexao == null) {
+      String path = join(await getDatabasesPath(), 'banco.db');
+
+      _conexao = await openDatabase(
+        path,
+        version: 1,
+        onCreate: (db, version) {
+          for (var comando in criar_banco) {
+            db.execute(comando);
+          }
+        },
+      );
+    }
+    return _conexao!;
+  }
+}
